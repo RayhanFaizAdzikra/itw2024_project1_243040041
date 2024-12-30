6f0bfef1eea81bd9a25e0b31ab99a3e0ab479e2e
RayhanFaizAdzikra <rayhankng266@gmail.com>
2024-12-30 15:48:27 +0700

    progres8

--- js/script.js
+++ js/script.js
@@ -1,19 +1,10 @@
-document.querySelectorAll("nav ul li a").forEach(link => {
-  link.addEventListener("click", function(e) {
-    e.preventDefault(); // Mencegah perilaku default navigasi
+const resumeBtns = document.querySelectorAll('.resume-btn')
 
-    // Hapus kelas "active" dari semua menu
-    document.querySelectorAll("nav ul li").forEach(li => li.classList.remove("active"));
-    this.parentElement.classList.add("active");
-
-    // Hapus kelas "active" dari semua section
-    document.querySelectorAll("section").forEach(sec => sec.classList.remove("active"));
-
-    // Tambahkan kelas "active" pada section yang sesuai
-    const target = document.querySelector(this.getAttribute("href"));
-    if (target) {
-      target.classList.add("active");
-      window.scrollTo({ top: target.offsetTop, behavior: "smooth" });
-    }
-  });
+resumeBtns.forEach((btn, idx) => {
+  btn.addEventListener('click', () => {
+    resumeBtns.forEach(btn => {
+      btn.classList.remove('active');
+    });
+    btn.classList.add('active');
+  });
 });
